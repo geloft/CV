23f3324814c8c8848a800432fe6aa2efc58a6c7e
Hryhoriy K <grisha.kutyanski@gmail.com>
2022-08-03 16:24:10 +0300

    Added data models User and Message

---
+++ CV/CV/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace CV.Models
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string Text { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ CV/CV/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace CV.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int MessageID { get; set; }
+        public ICollection<Message> Messages { get; set; }
+    }
+}

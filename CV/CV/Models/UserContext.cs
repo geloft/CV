dfb63bc73ce3be5a15e4bf8192792d07daa4a23b
Hryhoriy K <grisha.kutyanski@gmail.com>
2022-08-03 18:08:59 +0300

    Added DbContext

---
+++ CV/CV/Models/UserContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CV.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() : base()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MyCV-DB;Trusted_Connection=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Messages)
+                .WithOne(e => e.User)
+                .HasForeignKey(k=> k.MessageId);
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+}

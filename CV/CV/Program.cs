ea6c92d4a8c01cf4fb5df854ae4f384e8b2a6828
Hryhoriy K <grisha.kutyanski@gmail.com>
2022-08-03 18:55:12 +0300

    Add connection for Db

--- CV/CV/Program.cs
+++ CV/CV/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<CV.Models.UserContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 

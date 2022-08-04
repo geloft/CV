80b58c27a3685fc5127681f44d3b222eda9dd593
Hryhoriy K <grisha.kutyanski@gmail.com>
2022-08-04 22:08:35 +0300

    Set a unique field Email
    Also updated relationship

--- CV/CV/Models/UserContext.cs
+++ CV/CV/Models/UserContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CV.Models.DB.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CV.Models
 {
@@ -13,7 +14,9 @@ namespace CV.Models
             modelBuilder.Entity<User>()
                 .HasMany(u => u.Messages)
                 .WithOne(e => e.User)
-                .HasForeignKey(k=> k.MessageId);
+                .HasForeignKey(k => k.UserID);
+
+            modelBuilder.Entity<User>().HasIndex(m => m.Email).IsUnique();
         }
         public DbSet<User> Users { get; set; }
         public DbSet<Message> Messages { get; set; }
